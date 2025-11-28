882e84fdc82cd0b186ab1a2d69063b26dd1b4a51
richmondafari <afaririchmond1000@gmail.com>
2025-11-28 07:30:26 +0000

    feat(auth): implement JWT authentication flow

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User, { IUser } from "../models/User.js";
+import { unsubscribe } from "diagnostics_channel";
 
 // Register a new user
 export async function RegisterUser(req: Request, res: Response) {
@@ -46,6 +47,48 @@ export async function RegisterUser(req: Request, res: Response) {
       },
       token,
     });
+  } catch (error) {
+    return res.status(500).json({ message: "Server error", error });
+  }
+}
+
+// Login a user
+
+export async function LoginUser(req: Request, res: Response) {
+  try {
+    const { email, password } = req.body as { email: string; password: string };
+
+    // validate fields
+
+    if (!email || !password) {
+        return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const user =  await User.findOne( {email})
+    if (!user) {
+        return res.status(401).json( {message: "Invalid email or password"});
+    }
+
+    const isPasswordValid  =  await bcrypt.compare(password, user.passwordHash);
+    if (!isPasswordValid) {
+        return res.status(401).json( {message: "Invalid email or password"})
+    }
+
+   const token = jwt.sign(
+    { userId: user._id },
+    process.env["JWT_SECRET"] as string,
+    { expiresIn: "7d" }
+   );
+
+   return res.status(200).json({
+     message: "Login successful",
+     user: {
+       id: user._id,
+       name: user.name,
+       email: user.email,
+     },
+     token,
+   });
   } catch (error) {
     return res.status(500).json({ message: "Server error", error });
   }
